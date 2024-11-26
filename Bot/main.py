009d533ed63fa555419b90e32fedb48cbac2385e
lyszt <kalliddel@gmail.com>
2024-11-26 14:14:48 -0300

    who is command does a bing search

--- Bot/main.py
+++ Bot/main.py
@@ -146,7 +146,7 @@ async def whois(interaction: discord.Interaction, target: discord.Member):
     import requests
     from bs4 import BeautifulSoup
 
-    url = f"https://www.bing.com/search?q=\"aldynor\""
+    url = f"https://www.bing.com/search?q=\"{target.name}\""
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
